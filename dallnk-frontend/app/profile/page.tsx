6fee3b39f6047f7b443db39948aa4a4deb714ef2
cridiv <ademoladeremi0@gmail.com>
2025-09-26 21:17:34 +0100

    feat: Add Navbar and Footer in Profile Page

--- dallnk-frontend/app/profile/page.tsx
+++ dallnk-frontend/app/profile/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ProfilePage from "./Profile";
+import Navbar from "../home/components/Nav";
+import Footer from "../components/Footer";
 
 const page = () => {
   return (
     <div>
+      <Navbar />
       <ProfilePage />
+      <Footer />
     </div>
   );
 };
